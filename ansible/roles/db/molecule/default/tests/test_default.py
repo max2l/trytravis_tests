5a7984896a8b0486d60130c5bce80fcf41348527
Maksim Gorbachev <bezmenovo@gmail.com>
2018-04-19 14:44:26 +0300

    Install DB role

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -5,6 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 # check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
     mongo = host.service("mongod")
@@ -12,13 +13,7 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_enabled
 
 # check if configuration file contains the required line
-def test_config_file(host):
-    config_file = host.file('/etc/mongod.conf')
+def test_config_file(File):
+    config_file = File('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
-
-# check listet port
-def test_mongo_port(host):
-    listen_socket = host.socket("tcp://0.0.0.0:27017")
-    assert listen_socket.is_listening
-
