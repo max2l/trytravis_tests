32917bee2e73597bd18495249187f8adf93d6c19
Maksim Gorbachev <bezmenovo@gmail.com>
2018-04-22 22:50:18 +0300

    Create config files for terraform

---
+++ docker-monolith/infra/terraform/main.tf
@@ -0,0 +1,37 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "docker" {
+  name         = "reddit-docker-${count.index}"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+
+  count = "${var.count_servers}"
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  tags = ["reddit-docker"]
+}
+
