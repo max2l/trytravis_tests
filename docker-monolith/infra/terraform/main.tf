fdcccd40035bcbb0fae1b5ea03a6548389ba535e
Maksim Gorbachev <bezmenovo@gmail.com>
2018-04-23 13:19:52 +0300

    Add config files for Ansible

--- docker-monolith/infra/terraform/main.tf
+++ docker-monolith/infra/terraform/main.tf
@@ -4,12 +4,12 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_instance" "docker" {
-  name         = "reddit-docker-${count.index}"
+resource "google_compute_instance" "db" {
+  name         = "reddit-db"
   machine_type = "g1-small"
   zone         = "${var.zone}"
+  tags = ["reddit-db"]
 
-  count = "${var.count_servers}"
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
@@ -31,7 +31,42 @@ resource "google_compute_instance" "docker" {
     network       = "default"
     access_config = {}
   }
-
-  tags = ["reddit-docker"]
 }
 
+resource "google_compute_instance" "docker" {
+  name         = "reddit-docker-${count.index}"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags = ["reddit-docker"]
+
+  count = "${var.count_servers}"
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-docker"]
+}
