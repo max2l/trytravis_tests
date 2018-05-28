b4176e42b782b443a645524331da83c94971974d
Maksim Gorbachev <bezmenovo@gmail.com>
2018-05-28 12:39:42 +0300

    trytravis-2018-05-28T12:39:42.310940

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xe
+
+sudo wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+sudo chmod +x /usr/local/bin/gitlab-runner
+curl -sSL https://get.docker.com/ | sh
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
+sudo /usr/local/bin/gitlab-runner register \
+  --non-interactive \
+  --url "http://35.195.126.79/" \
+  --registration-token "dEsoJefiXvRX4txkoQHs" \
+  --executor "docker" \
+  --docker-image alpine:latest \
+  --description "docker-runner" \
+  --tag-list "linux,xenial,ubuntu,docker" \
+  --run-untagged \
+  --locked="false"
+
